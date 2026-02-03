fcede7ed4eb60f3d6de154f5cd4fa4897c19b90b
DevBharath-cyber <devbharathkathirvel@gmail.com>
2026-02-03 22:37:14 +0530

    frontend & backend add on env

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function App() {
+  return (
+    <div>
+      <p className="display-4 text-danger text-center font-semibold">
+        welcome to my website
+      </p>
+    </div>
+  );
+}
+
+export default App;
